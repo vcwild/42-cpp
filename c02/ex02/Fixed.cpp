4fa3c0e8dc17a6693ff752c305866ab75e512683
Victor Wildner <vwildner@redhat.com>
2022-10-08 16:45:35 -0300

    feat: implement c02 ex02

---
+++ c02/ex02/Fixed.cpp
@@ -0,0 +1,177 @@
+#include "Fixed.hpp"
+#include <cmath>
+#include <iostream>
+
+float Fixed::_getFractionalLeftShift( void ) const
+{
+    return ( float ) ( 1 << this->_fractionalBits );
+}
+
+Fixed::Fixed()
+{
+    this->_rawBits = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( Fixed const &instanceToCopy )
+{
+    *this = instanceToCopy;
+    std::cout << "Copy constructor called" << std::endl;
+}
+Fixed::Fixed( int const value )
+{
+    this->_rawBits = value << this->_fractionalBits;
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed( float const value )
+{
+    this->_rawBits = roundf( value * this->_getFractionalLeftShift() );
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_rawBits;
+}
+void Fixed::setRawBits( int const raw ) { this->_rawBits = raw; }
+
+float Fixed::toFloat( void ) const
+{
+    return ( ( float ) this->_rawBits ) / this->_getFractionalLeftShift();
+}
+int Fixed::toInt( void ) const
+{
+    return this->_rawBits >> this->_fractionalBits;
+}
+
+Fixed &Fixed::operator=( Fixed const &right )
+{
+    this->_rawBits = right.getRawBits();
+    std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+std::ostream &operator<<( std::ostream &output, Fixed const &value )
+{
+    output << value.toFloat();
+    return output;
+}
+
+Fixed Fixed::operator+( Fixed const &right )
+{
+    int sum = this->getRawBits() + right.getRawBits();
+    this->setRawBits( sum );
+    return *this;
+}
+
+Fixed Fixed::operator-( Fixed const &right )
+{
+    int sum = this->getRawBits() - right.getRawBits();
+    this->setRawBits( sum );
+    return *this;
+}
+
+Fixed Fixed::operator*( Fixed const &right )
+{
+    long sum = ( long ) this->getRawBits()
+        * ( long ) ( right.getRawBits() / this->_getFractionalLeftShift() );
+    this->setRawBits( ( int ) sum );
+    return *this;
+}
+
+Fixed Fixed::operator/( Fixed const &right )
+{
+    long sum = ( long ) this->getRawBits()
+        * ( ( long ) this->_getFractionalLeftShift()
+            / ( long ) right.getRawBits() );
+    this->setRawBits( ( int ) sum );
+    return *this;
+}
+
+Fixed Fixed::operator++( int )
+{
+    Fixed instance = *this;
+    this->_rawBits++;
+    return instance;
+}
+
+Fixed &Fixed::operator++( void )
+{
+    this->_rawBits++;
+    return *this;
+}
+
+Fixed Fixed::operator--( int )
+{
+    Fixed instance = *this;
+    this->_rawBits--;
+    return instance;
+}
+
+Fixed &Fixed::operator--( void )
+{
+    this->_rawBits--;
+    return *this;
+}
+
+bool Fixed::operator>( Fixed const &right ) const
+{
+    return this->getRawBits() > right.getRawBits();
+}
+
+bool Fixed::operator<( Fixed const &right ) const
+{
+    return this->getRawBits() < right.getRawBits();
+}
+
+bool Fixed::operator>=( Fixed const &right ) const
+{
+    return this->getRawBits() >= right.getRawBits();
+}
+
+bool Fixed::operator<=( Fixed const &right ) const
+{
+    return this->getRawBits() <= right.getRawBits();
+}
+
+bool Fixed::operator==( Fixed const &right ) const
+{
+    return this->getRawBits() == right.getRawBits();
+}
+
+bool Fixed::operator!=( Fixed const &right ) const
+{
+    return this->getRawBits() != right.getRawBits();
+}
+
+Fixed const &Fixed::min( Fixed const &left, Fixed const &right )
+{
+    if ( left > right )
+        return right;
+    return left;
+}
+
+Fixed &Fixed::min( Fixed &left, Fixed &right )
+{
+    if ( left > right )
+        return right;
+    return left;
+}
+
+Fixed const &Fixed::max( Fixed const &left, Fixed const &right )
+{
+    if ( left > right )
+        return left;
+    return right;
+}
+
+Fixed &Fixed::max( Fixed &left, Fixed &right )
+{
+    if ( left > right )
+        return left;
+    return right;
+}
---
+++ c02/ex02/Fixed.hpp
@@ -0,0 +1,52 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+
+class Fixed {
+
+public:
+    Fixed();
+    Fixed( int const value );
+    Fixed( float const value );
+    Fixed( Fixed const &instanceToCopy );
+    ~Fixed();
+
+    int   getRawBits( void ) const;
+    void  setRawBits( int const raw );
+    float toFloat( void ) const;
+    int   toInt( void ) const;
+
+    Fixed &operator=( Fixed const &right );
+    Fixed  operator+( Fixed const &right );
+    Fixed  operator-( Fixed const &right );
+    Fixed  operator*( Fixed const &right );
+    Fixed  operator/( Fixed const &right );
+
+    Fixed  operator++( int );
+    Fixed &operator++( void );
+    Fixed  operator--( int );
+    Fixed &operator--( void );
+
+    bool operator>( Fixed const &right ) const;
+    bool operator<( Fixed const &right ) const;
+    bool operator>=( Fixed const &right ) const;
+    bool operator<=( Fixed const &right ) const;
+    bool operator==( Fixed const &right ) const;
+    bool operator!=( Fixed const &right ) const;
+
+    static Fixed       &min( Fixed &left, Fixed &right );
+    static const Fixed &min( Fixed const &left, Fixed const &right );
+    static Fixed       &max( Fixed &left, Fixed &right );
+    static const Fixed &max( Fixed const &left, Fixed const &right );
+
+private:
+    int              _rawBits;
+    static const int _fractionalBits = 8;
+
+    float _getFractionalLeftShift( void ) const;
+};
+
+std::ostream &operator<<( std::ostream &output, Fixed const &value );
+
+#endif
