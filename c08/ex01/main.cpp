f22f72009028660486a2c2c493230a6e34b5199c
Victor Wildner <vwildner@redhat.com>
2022-10-18 23:07:43 -0300

    fix: update tests

--- c08/ex01/main.cpp
+++ c08/ex01/main.cpp
@@ -6,16 +6,18 @@
 
 void show( std::string const &name, Span &span )
 {
+    std::cout << name << "\t";
     span.printItems();
-    std::cout << name << ": ( shortest - " << span.shortestSpan()
-              << ", longest - " << span.longestSpan() << " )" << std::endl;
+    std::cout << name << "\tSpan: shortest: " << span.shortestSpan()
+              << " | longest: " << span.longestSpan() << std::endl
+              << std::endl;
 }
 
 int main( void )
 {
     std::srand( time( NULL ) );
 
-    Span span = Span( 5 );
+    Span span( 5 );
     span.addNumber( 5 );
     span.addNumber( 3 );
     span.addNumber( 17 );
@@ -23,5 +25,23 @@ int main( void )
     span.addNumber( 11 );
     show( "5 Numbers", span );
 
+    {
+        Span spanCopy( span );
+        show( "Copy", spanCopy );
+        try {
+            spanCopy.addNumber( 23 );
+        } catch ( std::exception &e ) {
+            std::cout << e.what() << std::endl << std::endl;
+        }
+    }
+    {
+        Span spanAssign = span;
+        show( "Assign", spanAssign );
+        try {
+            spanAssign.addNumber( 23 );
+        } catch ( std::exception &e ) {
+            std::cout << e.what() << std::endl << std::endl;
+        }
+    }
     return 0;
 }
